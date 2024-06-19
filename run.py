1cca399d5c993cea0dda374c3aeb8bd6783ed345
Samuel-Frost <149511278+Samuel-Frost@users.noreply.github.com>
2024-06-19 19:25:53 +0100

    Export NEB .extxyz

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from ase.io import read
+from ase.io import read, write
 from read import read_log
 from generate_structures import generate_diamond, generate_neb, find_middle
 from convert import convert
@@ -19,8 +19,8 @@ sys("mpirun -np 36 lmp_mpi -in diamond.in")
 print("GENERATING NEB FILES")
 middle = find_middle(size)
 
-def run(index):
-    trans = generate_neb(size, index)
+def run(index, layer_number):
+    trans = generate_neb(size, index, layer_number) # all the naming is done here
     print("MINIMISING NEB STRUCTURES")
     sys("mpirun -np 36 lmp_mpi -in min.in")
     print("CONVERTING TO NEB FORMAT")
@@ -28,6 +28,18 @@ def run(index):
     # CALL NEB.IN
     print("RUNNING NEB")
     sys("mpirun -np 36 lmp_mpi -partition 9x4 -in neb.in")
+    # ONCE NEB IS DONE READ IN AND REWRITE FILES WITH DIFFERENT NAME 
+    struct = read('1_rel.lammpstrj', index='-1')
+    write(f'rel_{layer_number * 2 - 1}.xyz', struct)
+    struct = read('2_rel.lammpstrj', index='-1')
+    write(f'rel_{layer_number * 2}.xyz', struct)
+
+    traj = []
+    for i in range(1, 10):
+        traj.append(read(f'dump.neb.{i}', index=-1))
+
+    write(f'neb_{layer_number}.extxyz', traj)
+
     return trans
 
 def write_csv(i):
@@ -42,16 +54,16 @@ def write_csv(i):
 Ebf = []
 Ebr = []
 
-trans = run(middle)
+trans = run(middle, 1)
 write_csv(0)
-for i in range(1, h*2-1):
+for i in range(2, h*2):
     print(trans)
-    trans = run(trans)
+    trans = run(trans, i)
     write_csv(i)
 
 
 #np.savetxt('convergence', [h, Ebf, Ebr])
+plt.style.use('seaborn-v0_8')
 plt.plot(Ebf)
 plt.plot(Ebr)
-plt.style.use('seaborn-v0_8')
 plt.savefig('convergence.png', dpi=600)
